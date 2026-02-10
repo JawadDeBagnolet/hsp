c7852fb05e333570e93290f1007474a205bc4115
Jonathan BAO <BAO_Jo@lprs.local>
2026-02-10 11:38:31 +0100

    Restaurer interface page accueil

--- src/main/java/appli/hsp/HelloController.java
+++ src/main/java/appli/hsp/HelloController.java
@@ -39,18 +39,23 @@ public class HelloController {
                 UserRepository userRepository = new UserRepository();
                 User utilisateur = userRepository.trouverUtilisateurParEmail(email);
                 
+                System.out.println("Recherche utilisateur pour email: " + email);
+                
                 if (utilisateur != null) {
+                    System.out.println("Utilisateur trouvé: " + utilisateur.getNom() + " " + utilisateur.getPrenom());
                     // Stocker l'utilisateur dans la session
                     SessionManager.setUtilisateurConnecte(utilisateur);
                     
                     // Redirection vers la page d'accueil
                     StartApplication.changeScene("pageAccueil");
                 } else {
-                    erreurLabel.setText("Erreur: Utilisateur non trouvé");
+                    System.err.println("Utilisateur non trouvé pour email: " + email);
+                    erreurLabel.setText("Erreur: Utilisateur non trouvé après vérification");
                 }
             } catch (Exception e) {
-                erreurLabel.setText("Erreur lors de la redirection");
+                System.err.println("Exception lors de la redirection: " + e.getMessage());
                 e.printStackTrace();
+                erreurLabel.setText("Erreur lors de la redirection: " + e.getMessage());
             }
         } else {
             erreurLabel.setText("Identifiant ou mot de passe incorrect");
