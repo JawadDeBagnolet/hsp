9d47a797046e1dc9882ba8d2d219b3ea1d3cfbf4
Sohaib HAMDI <HAMDI_So@lprs.local>
2026-03-02 16:36:24 +0100

    Modification VerifConnexion & PageUtilisateurs

--- src/main/java/appli/hsp/VerifConnexion.java
+++ src/main/java/appli/hsp/VerifConnexion.java
@@ -1,42 +1,51 @@
 package appli.hsp;
-
 import database.Database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import appli.util.PasswordUtils;
 public class VerifConnexion {
-    
+
     public static boolean verifierConnexion(String email, String motDePasse) {
+
         Connection cnx = Database.getConnexion();
+
         if (cnx == null) {
             System.out.println("Connexion à la base de données échouée");
             return false;
         }
-        
-        String query = "SELECT * FROM user WHERE email = ? AND mdp = ?";
-        
+
+        String query = "SELECT mdp FROM user WHERE email = ?";
+
         try (PreparedStatement stmt = cnx.prepareStatement(query)) {
+
             stmt.setString(1, email);
-            stmt.setString(2, motDePasse);
-            
+
             ResultSet rs = stmt.executeQuery();
-            boolean connexionReussie = rs.next();
-            
+
+            if (rs.next()) {
+
+                String hashedPassword = rs.getString("mdp");
+
+                boolean connexionReussie =
+                        PasswordUtils.checkPassword(motDePasse, hashedPassword);
+
+                rs.close();
+                stmt.close();
+                cnx.close();
+
+                return connexionReussie;
+            }
+
             rs.close();
             stmt.close();
             cnx.close();
-            
-            return connexionReussie;
-            
+
+            return false;
+
         } catch (SQLException e) {
-            System.out.println("Erreur lors de la vérification des identifiants: " + e.getMessage());
-            try {
-                cnx.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
+            e.printStackTrace();
             return false;
         }
     }
